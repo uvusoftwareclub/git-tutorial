9e36c3462943bbf024035e21b4cab608863c2136
handsofblue67 <michael.robison168@gmail.com>
2016-04-13 16:44:21 -0600

    added lodash and some io

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /**
  * Remember to always comment your code!!!
  */
+var _ = require('lodash');
+
 console.log("Hello Git!");
 
+var input = [1, 2, 4, 53, 134, 43];
 
-
+_.each(input, o => console.log(o) );
